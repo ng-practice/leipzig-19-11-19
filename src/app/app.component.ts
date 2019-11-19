10905304cf81d68d8e8e766ccff0037b43b0ddc7
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-11-19 16:20:19 +0100

    add props to appComnponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'porsche';
+  title = 'Porsche';
+  hobbys = 'Holz hacken.';
 }
