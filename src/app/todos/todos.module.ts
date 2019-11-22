3587f98570a3a5d2988079b2fb74f825fc127b22
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-11-22 08:41:29 +0100

    chore: register http

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { TodoCheckerComponent } from './todo-checker/todo-checker.component';
 import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
 import { TodosComponent } from './todos.component';
 
 @NgModule({
   declarations: [TodoCheckerComponent, TodosComponent, TodoQuickAddComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [TodosComponent]
 })
 export class TodosModule {}
