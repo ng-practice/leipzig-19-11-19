2192da8ef023d594f0d6c962bc0d3c0f99d31201
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-12-08 21:09:39 +0100

    remove forms module

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SortByTextPipe } from './shared/sort-by-text.pipe';
 import { TodoCheckerComponent } from './todo-checker/todo-checker.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
 import { TodosLinkNavigationComponent } from './todos-link-navigation/todos-link-navigation.component';
 import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './todos.component';
-import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +18,7 @@ import { TodoEditComponent } from './todo-edit/todo-edit.component';
     TodosLinkNavigationComponent,
     TodoEditComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    TodosRoutingModule
-  ],
+  imports: [CommonModule, HttpClientModule, TodosRoutingModule],
   exports: [TodosComponent]
 })
 export class TodosModule {}
