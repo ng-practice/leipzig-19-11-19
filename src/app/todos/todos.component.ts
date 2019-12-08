21422f011edf2b015e18b027e75f43673ed6c531
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-12-08 18:17:43 +0100

    fix: get filter to work

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,37 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import { Todo } from './models/todo';
 import { TodosService as TodosApi } from './shared/todos-api.service';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'ws-todos',
   templateUrl: './todos.component.html'
 })
-export class TodosComponent implements OnInit {
-  todos: Observable<Todo[]>;
+export class TodosComponent implements OnInit, OnDestroy {
+  destroy = new Subject<void>();
+  todos: Todo[] = [];
 
   constructor(private route: ActivatedRoute, private todoApi: TodosApi) {}
 
   ngOnInit(): void {
-    this.todos = this.route.paramMap.pipe(
-      switchMap(paramMap => this.todoApi.query(paramMap.get('query')))
-    );
+    this.route.paramMap
+      .pipe(
+        switchMap(paramMap => this.todoApi.query(paramMap.get('query'))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(todos => (this.todos = todos));
   }
 
   create(todoForCreation: Todo): void {
-    this.todos = this.todoApi.create(todoForCreation).pipe(
-      switchMap(() => this.route.paramMap),
-      switchMap(paramMap => this.todoApi.query(paramMap.get('query')))
-    );
+    this.todoApi
+      .create(todoForCreation)
+      .pipe(
+        switchMap(() => this.route.paramMap),
+        switchMap(paramMap => this.todoApi.query(paramMap.get('query'))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(todos => (this.todos = todos));
   }
 
   delete(todoForDeletion: Todo): void {
-    this.todos = this.todoApi.delete(todoForDeletion);
+    this.todoApi
+      .delete(todoForDeletion)
+      .pipe(
+        switchMap(() => this.route.paramMap),
+        switchMap(paramMap => this.todoApi.query(paramMap.get('query'))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(todos => (this.todos = todos));
   }
 
   checkOrUncheck(todoForUpdate: Todo): void {
-    this.todos = this.todoApi.checkOrUncheck(todoForUpdate);
+    this.todoApi
+      .checkOrUncheck(todoForUpdate)
+      .pipe(
+        switchMap(() => this.route.paramMap),
+        switchMap(paramMap => this.todoApi.query(paramMap.get('query'))),
+        takeUntil(this.destroy)
+      )
+      .subscribe(todos => (this.todos = todos));
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
