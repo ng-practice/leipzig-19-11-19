6a78e14dcc255b9f401a46787489ae482e62281d
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-12-09 16:17:43 +0100

    test: provide pipe tests

--- src/app/todos/shared/sort-by-text.pipe.spec.ts
+++ src/app/todos/shared/sort-by-text.pipe.spec.ts
@@ -1,12 +1,6 @@
 import { Todo } from '../models/todo';
 import { SortByTextPipe } from './sort-by-text.pipe';
 
-/*
- * When an unsorted list is given, it yields a sorted list by alphabet.
- * When noting is given, it yields an empty list.
- * When a number is given, it yields an empty list.
- */
-
 describe('Pipe: sortByText', () => {
   describe('When an unsorted list is given', () => {
     it('yields a sorted list by alphabet', () => {
@@ -20,6 +14,28 @@ describe('Pipe: sortByText', () => {
 
       expect(sorted[0].text).toBe('a');
       expect(sorted[1].text).toBe('b');
+    });
+  });
+
+  describe('When nothing is given', () => {
+    it('yields an empty list', () => {
+      const pipe = new SortByTextPipe();
+      const nothing = null;
+
+      const result = pipe.transform(nothing, 'asc');
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('When a number is given', () => {
+    it('yields an empty list', () => {
+      const pipe = new SortByTextPipe();
+      const three = 3;
+
+      const result = pipe.transform(three as any, 'asc');
+
+      expect(result).toEqual([]);
     });
   });
 });
