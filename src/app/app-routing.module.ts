81564be88534a1587c004c9c2edc07b5dd81125a
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-12-09 09:57:36 +0100

    feat: use lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutModule } from './about/about.module';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'todos/all' }
+  { path: '', pathMatch: 'full', redirectTo: 'todos/all' },
+  {
+    path: 'about',
+    // loadChildren:  './about/about.module#AboutModule' // Angular < 8
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), AboutModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
