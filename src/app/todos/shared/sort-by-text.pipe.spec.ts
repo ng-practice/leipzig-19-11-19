7b113a21e2e875f4331c9e347b48067158c85ddd
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-12-10 11:03:38 +0100

    ref: use beforeEach

--- src/app/todos/shared/sort-by-text.pipe.spec.ts
+++ src/app/todos/shared/sort-by-text.pipe.spec.ts
@@ -2,14 +2,18 @@ import { Todo } from '../models/todo';
 import { SortByTextPipe } from './sort-by-text.pipe';
 
 describe('Pipe: sortByText', () => {
+  let pipe: SortByTextPipe;
+
+  beforeEach(() => {
+    pipe = new SortByTextPipe();
+  });
+
   describe('When an unsorted list is given', () => {
     it('yields a sorted list by alphabet', () => {
-      const pipe = new SortByTextPipe();
       const unsorted: Todo[] = [
         { text: 'b', isDone: true },
         { text: 'a', isDone: true }
       ];
-
       const sorted = pipe.transform(unsorted, 'asc');
 
       expect(sorted[0].text).toBe('a');
@@ -19,9 +23,7 @@ describe('Pipe: sortByText', () => {
 
   describe('When nothing is given', () => {
     it('yields an empty list', () => {
-      const pipe = new SortByTextPipe();
       const nothing = null;
-
       const result = pipe.transform(nothing, 'asc');
 
       expect(result).toEqual([]);
@@ -30,9 +32,7 @@ describe('Pipe: sortByText', () => {
 
   describe('When a number is given', () => {
     it('yields an empty list', () => {
-      const pipe = new SortByTextPipe();
       const three = 3;
-
       const result = pipe.transform(three as any, 'asc');
 
       expect(result).toEqual([]);
